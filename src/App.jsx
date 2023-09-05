cccff423535cef31c041807870b5ec2c0fc3bbe7
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-05 23:27:05 +0200

    useSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useEffect } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/results.json";
+import { useState } from "react";
+import { useId } from "react";
+
+function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(false);
+  const isFirstInput = useId(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+
+    if (search === "") {
+      setError("La búsqueda no puede estar vacía");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(false);
+  }, [search, isFirstInput]);
+
+  return { search, updateSearch, error };
+}
 
 function App() {
+  const { search, updateSearch, error } = useSearch();
   const movies = responseMovies.Search;
 
   const mappedMovies = movies.map((movie) => {
